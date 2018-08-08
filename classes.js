08af5afd440d78a791e07dff3b304cb9e4fb14f9
Ashley Nguyen <ashleygnguyen@yahoo.com>
2018-08-08 16:01:23 -0500

    Finished edit

--- classes.js
+++ classes.js
@@ -63,8 +63,25 @@ console.log(employee1)
 
 //Code Here
 
-var manager = Object.assign({}, first_name, last_name, email, age)
-
+class Manager{
+constructor(first_name, last_name, email, age, reports){
+  this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+    this.reports = []
+}
+makeWidget(){
+  return `${this.first_name} ${this.last_name} Widget`
+}
+hire(newEmployee){
+  this.reports.push(newEmployee)
+}
+fire(newEmployee){
+  this.reports.splice(newEmployee,1)
+}
+}
+const manager1 = new Manager("Ashley", "Nguyen", "agnguyen@yahoo.com", 24, [1,2,3])
 
 ////////// PROBLEM 3 //////////
 
@@ -89,7 +106,41 @@ var manager = Object.assign({}, first_name, last_name, email, age)
 */
 
 //Code Here
-
+class ProgressiveManager{
+  constructor(first_name, last_name, email, age, reports, title, bonus){
+    this.first_name = first_name
+      this.last_name = last_name
+      this.email = email
+      this.age = age
+      this.reports = []
+      this.title = 'Not a manager'
+      this.bonus = 0
+  }
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+  hire(newEmployee){
+    this.reports.push(newEmployee)
+    if (this.reports.length === 0){
+      this.title = 'Not a manager'
+    } else if (this.reports.length >=1 && this.reports.length <= 3){
+      this.title = 'Barely Manager'
+    } else if (this.reports.length >=4 && this.reports.length <= 10){
+      this.title = 'Mostly Manager'
+    }else if (this.reports.length >=11 && this.reports.length <= 50){
+      this.title = 'Manager'
+    }else if (this.reports.length >=51 && this.reports.length <= 100){
+      this.title = 'Manager Plus'
+    }else if (this.reports.length >=101){
+      this.title = 'Bestest Manager'
+    }
+  }
+  fire(newEmployee){
+    this.reports.splice(newEmployee,1)
+    this.bonus += 100 
+  }
+  }
+  const progmanager1 = new ProgressiveManager("Ashley", "Nguyen", "agnguyen@yahoo.com", 24, [1,2,3], 'Not a manager', 0)
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
@@ -117,4 +168,24 @@ var manager = Object.assign({}, first_name, last_name, email, age)
 
 //Code Here
 
+class Machine{
+  constructor(widgets_made_count, wear_and_tear_count, needs_reboot){
+    this.widgets_made_count = 0
+    this.wear_and_tear_count = 0
+    this.needs_reboot = false
+  }
+  makeWidgets(num){
+    this.widgets_made_count += num
+      this.wear_and_tear_count += num/50
+    }
+  
+  fixMachine(){
+    this.needs_reboot = true
+  }
+  reboot(){
+    this.wear_and_tear_count -= 10
+    this.needs_reboot = false
+    return function rebootComplete(){}
+  }
+}
 
